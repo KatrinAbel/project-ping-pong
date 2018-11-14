33da36847d868e39f2920191b7496149d3a4b811
Katrin Abel <katrin@abel-essen.de>
2018-11-14 19:45:17 +0100

    HERRRROOOKKKUUU

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const flash      = require("connect-flash");
     
 
 mongoose
-  .connect('mongodb://localhost/project-ping-pong', {useNewUrlParser: true})
+  .connect('process.env.MONGODB_URI', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
